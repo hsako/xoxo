62f556046298aa43ba5845a2e48a13327bc4c828
Hawa Sako <Hawa.d.sako@gmail.com>
2018-07-05 14:57:55 -0400

    changes

--- game/index.js
+++ game/index.js
@@ -1,6 +1,62 @@
-import {Map} from 'immutable'
+//questions: how do we know what to export?!??!
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+
+
+import {Map} from 'immutable';
+
+// export default function reducer(state, action) {
+
+
+export const MOVE = 'MOVE';
+
+export const move = (player, position) => ({
+	type: MOVE,
+	position,
+	player
+});
+
+
+function turn (current = 'X', action) {
+	if (action.type === MOVE){
+		return current === 'X' ? 'O' : 'X';
+	} else {
+		return current;
+	}
 }
+
+
+function board (board = Map(), {type, position, player}){
+	if (type === MOVE){
+		return board.setIn(position, player);
+	} else {
+		return board;
+	}
+}
+
+// function streak(){
+
+// }
+
+// export function winner(){
+
+// }
+
+export default function reducer(state = {}, action){
+
+
+}
+	// const newState = Object.assign({}, state)
+	// 	switch(action.type){
+	// 		case MOVE:
+
+	// 		board = newState.board
+	// 		turn = newState.turn
+	// 	}
+
+	// return {
+	// 	board: board(state.board,action),
+	// 	turn: turn(state.turn, action),
+	// }
+
+
+// }
